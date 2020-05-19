fbfdc077efe7a2314e0692c1a2b5e2a1d228908c
unknown <eric,maloney@centre.edu>
2020-05-19 01:37:53 -0400

    finallab4

--- camera.js
+++ camera.js
@@ -4,7 +4,7 @@ cameraPromise.then(function(cameras)
 {
     console.log("camera data",cameras);
     
-    var screen = {width:900, height:500};
+    var screen = {width:1000, height:500};
     
     var margins = {top:15,bottom:40,left:70,right:15};
    
@@ -20,16 +20,17 @@ cameraPromise.then(function(cameras)
             .range([0,graph.width])
 
         var yScale = d3.scaleLinear()
-            .domain([0,100])
+            .domain([0,250])
             .range([graph.height,0])
 
     var colors = ["gold", "silver", "red"]
     
     
-    var series = initGraph("svg",cameras,margins);
+    var series = initGraph("svg",cameras,margins,screen);
     drawStack(cameras,graph,xScale,yScale,series,colors)
-
-    
+    drawToolTip(drawStack)
+    createAxes(screen,margins,graph,target,xScale,yScale)
+    createLegend(cameras,screen,margins,graph,target,colors)
     
     
 
@@ -43,7 +44,7 @@ cameraPromise.then(function(cameras)
 
 
 
- var initGraph = function(target,cameras,margins)
+ var initGraph = function(target,cameras,margins,screen)
 {
   
     
@@ -66,11 +67,9 @@ cameraPromise.then(function(cameras)
     
     series = stack(cameras); 
     return series;
-}
- 
- 
- 
- 
+    
+};
+
  var drawStack = function(cameras,graph,xScale,yScale,series,colors)
  {
      console.log("series;")
@@ -91,7 +90,7 @@ cameraPromise.then(function(cameras)
            { 
                 return d; 
            })
-     .enter()    
+     .enter()
      .append("rect")    
      .attr("x", function(d, i) 
            {        
@@ -100,54 +99,96 @@ cameraPromise.then(function(cameras)
      .attr("y", function(d) 
            {       
          console.log("d=",d)
-         return graph.height- yScale(d[0]);
+         return yScale(d[1]);
            })    
      .attr("height", function(d) 
            {        
-         return yScale(d[1]-d[0]);
-          })    
+         return yScale(d[0])-yScale(d[1]);
+          }) 
+     .on("mouseover",drawToolTip)
+     .on("mouseout",function() 
+            {
+        d3.select("#tooltip")
+       .classed("hidden",true);
+            })
      .attr("width", graph.width/24);
-};
+     
+ }
  
- 
 
-
-
-/*
-var clearScatter = function(target)
+var drawToolTip = function(drawStack)
 {
-    d3.select(target)
-        .select("svg")
-        .selectAll("rect")
+    console.log("drawing");
+    
+    d3.select("#tooltip div")
         .remove();
+    
+    
+    var xPosition = d3.event.pageX;
+    var yPosition = d3.event.pageY;
+
+    
+    var base = d3.select("#tooltip")
+        .classed("hidden",false)
+        .style("top",yPosition+"px")
+        .style("left",xPosition+"px")
+        .append("div")
+        .text(drawStack[1])
+          
+};
+
+var createAxes = function(screen,margins,graph,
+                           target,xScale,yScale)
+{
+    var xAxis = d3.axisBottom(xScale);
+    var yAxis = d3.axisLeft(yScale);
+    
+    var axes = d3.select("graph")
+        .append("g")
+    axes.append("g")
+        .attr("transform","translate("+margins.left+","
+             +(margins.top+graph.height)+")")
+        .call(xAxis)
+    axes.append("g")
+        .attr("transform","translate("+margins.left+","
+             +(margins.top)+")")
+        .call(yAxis)
 }
 
-*/
 
 
-/*
-var tooltip = d3.select("svg")
-.append("text")
-.style("opacity", 0)
-.attr("class","tooltip")
 
-var mouseover = function(cameras)
+var createLegend = function(cameras,screen,margins,graph,
+                             target,colors)
 {
-    tooltip
-    .style("opacity",0)
-    .append("text")
-    .attr("src",function(cameras)
-      {
-    return "cameraPromise", ;
-});
+    
+    var legend = d3.select(target)
+        .append("g")
+        .classed("legend",true)
+        .attr("transform","translate("+
+              (margins.left+ 10) +","+
+             (margins.top+(graph.height/1.5))+")");
+    
+    var entries = legend.selectAll("g")
+        .data(["Megapixels","4K Video FPS","Slow Motion FPS"])
+        .enter()
+        .append("g")
+        .classed("legendEntry",true)
+        .attr("fill",function(d,i)
+             {
+                return colors[i];
+             })
+        .attr("transform",function(d,i)
+              {
+                return "translate(0,"+i*20+")";
+              })
+              
+        entries.append("rect")
+                .attr("width",10)
+                .attr("height",10)
+    
+        entries.append("text")
+                .text(function(cameras){return cameras;})
+                .attr("x",15)
+                .attr("y",10)
 }
-
-var mouseleave= function(cameras)
-{
- tooltip
- .style("opacity",0)
-*/
-
-
-
-
